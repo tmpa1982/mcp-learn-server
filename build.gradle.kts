5d09cb700acfc2b8dcdfaa66897516701121e419
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-10-18 12:15:34 +0800

    Add ktor dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,8 @@ repositories {
 dependencies {
     implementation("io.modelcontextprotocol:kotlin-sdk:${mcpVersion}")
     implementation("org.slf4j:slf4j-nop:${slf4jVersion}")
+    implementation("io.ktor:ktor-client-core:${ktorVersion}")
+    implementation("io.ktor:ktor-client-cio:${ktorVersion}")
     implementation("io.ktor:ktor-client-content-negotiation:${ktorVersion}")
     implementation("io.ktor:ktor-serialization-kotlinx-json:${ktorVersion}")
 
